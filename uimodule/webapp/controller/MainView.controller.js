173ab1d5008751651daa69f253b94321b24c7e83
Kiran Machineni <mkrn@live.in>
2021-03-25 18:58:52 +0530

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/sap/usingEvents/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.sap.usingEvents.controller.MainView", {});
+});
