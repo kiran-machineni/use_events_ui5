71935ecc2702adb47eb4af377d639b2567ceba46
Kiran Machineni <mkrn@live.in>
2021-03-25 20:27:06 +0530

    first commit

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,7 +1,20 @@
-sap.ui.define([
-  "com/sap/usingEvents/controller/BaseController"
-], function(Controller) {
-  "use strict";
+sap.ui.define(
+  ['com/sap/usingEvents/controller/BaseController', 'sap/m/MessageBox'],
+  function (Controller, MessageBox) {
+    'use strict'
 
-  return Controller.extend("com.sap.usingEvents.controller.MainView", {});
-});
+    return Controller.extend('com.sap.usingEvents.controller.MainView', {
+      onInit: function () {
+        this.getView().byId('idImg1').setSrc('./img/O_UI5_V_noBG.png')
+      },
+      onImg1Press: function () {
+        MessageBox.information('UI5 Logo Clicked', {
+          icon: MessageBox.Icon.INFORMATION,
+          title: 'Information'
+        })
+
+        // sap.m.MessageToast.show('icon clicked')
+      }
+    })
+  }
+)
